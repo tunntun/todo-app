5002a391671e23b04e5e9619edf468d7239bc148
tunntun <mervehtnozcan@gmail.com>
2025-09-02 12:30:55 +0300

    add task route

--- routes/taskRoute.js
+++ routes/taskRoute.js
@@ -1,3 +1,37 @@
 const express = require('express');
 
 const router = express.Router();
+
+const createTaskGetController = require('../controllers/task/create/get');
+const updateTaskGetController = require('../controllers/task/update/get');
+const deleteTaskGetController = require('../controllers/task/update/delete/get');
+
+const createTaskPostController = require('../controllers/task/create/post');
+const updateTaskPostController = require('../controllers/task/update/post');
+
+router.get(
+  'create',
+  createTaskGetController
+);
+
+router.get(
+  'update',
+  updateTaskGetController
+);
+
+router.get(
+  'update/delete',
+  deleteTaskGetController
+);
+
+router.post(
+  'create',
+  createTaskPostController
+);
+
+router.post(
+  'update',
+  updateTaskPostController
+);
+
+module.exports = router;
